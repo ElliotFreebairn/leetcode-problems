2edbe70eabccc89d865cf498380177b5795dfdcc
elliotfreebairn <elliotfreebairn1@gmail.com>
2025-07-07 20:57:35 +0100

    Day 10 of leetoced. Medium challenge done, still need to figure out c++ optimizations

---
+++ medium-238-move-zeros.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+class Solution
+{
+public:
+void moveZeroes(vector<int>& nums) {
+  int count = 0;
+    for (int i = 0; i < nums.size();) {
+      if (nums[i] == 0) {
+          nums.erase(nums.begin() + i);
+          count++;
+      } else {
+          i++;
+      }
+    }
+    nums.insert(nums.end(), count, 0);
+  }
+};
+
+int main() 
+{
+
+}
